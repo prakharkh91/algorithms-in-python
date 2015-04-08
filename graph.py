6244e994957faee895e37811e093489de2f3d81b
Prakhar Khandelwal Personal <prakhar.kh91@gmail.com>
2015-04-08 22:27:21 +0530

    Find all paths between two points implemented

--- graph.py
+++ graph.py
@@ -1,5 +1,7 @@
 ## We are using the edge list representation to 
 ## form a graph. 
+## If a node is not connected to any other nodes, we do not 
+## list it in the graph. Hence the second if condition.
 ## { 'a': ['b', 'c']}
 def find_path(graph, start, end, path=[]):
     path = path + [start]
@@ -13,6 +15,22 @@ def find_path(graph, start, end, path=[]):
             if newpath:
                 return newpath
     return None
+
+def find_all_paths(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return path
+    if not graph.has_key(start):
+        return None
+    paths = []
+    for node in graph[start]:
+        if node not in path:
+            newpath = find_path(graph, node, end, path)
+            if newpath:
+                paths.append(newpath)
+
+    return paths
+
 
 
 graph = {
@@ -28,3 +46,6 @@ graph = {
 print find_path(graph, 'B', 'F')
 print find_path(graph, 'A', 'F')
 print find_path(graph, 'A', 'D')
+
+print "All Paths between A & D"
+print find_all_paths(graph, 'A', 'D')
